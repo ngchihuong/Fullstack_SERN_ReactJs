13c5e3b5098fcfe60d21942cf74fcf7ba7bd7f28
Huongzzz <125900985+ngchihuong@users.noreply.github.com>
2024-05-22 00:23:35 +0700

    50 Hoàn Thiện Giao Diện Clone BookingCare.vn 3

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
 import Specialty from './Section/Specialty';
-
+import MedicalFacility from './Section/MedicalFacility';
+import './HomePage.scss';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import OutStandingDoctor from './Section/OutStandingDoctor';
+import HandBook from './Section/HandBook';
 
 class HomePage extends Component {
 
     render() {
-
+        let settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 2,
+        };
         return (
             <div>
                 <HomeHeader></HomeHeader>
-                <Specialty></Specialty>
-                <div style={{height:'100px'}}></div>
+                <Specialty settings = {settings}></Specialty>
+                <MedicalFacility settings = {settings}></MedicalFacility>
+                <OutStandingDoctor settings={settings}></OutStandingDoctor>
+                <HandBook settings={settings}></HandBook>
             </div>
         );
     }

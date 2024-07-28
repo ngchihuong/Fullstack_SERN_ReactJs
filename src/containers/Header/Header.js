99a8f016225b9a6f82e7c47b21f1280ee597d34c
Huongzzz <125900985+ngchihuong@users.noreply.github.com>
2024-07-28 23:00:51 +0700

    71 Tạo Menu Phân Quyền Người Dùng Khi Đăng Nhập

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -3,37 +3,62 @@ import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import * as actions from "../../store/actions";
 import Navigator from '../../components/Navigator';
-import { adminMenu } from './menuApp';
+import { adminMenu, doctorMenu } from './menuApp';
 import './Header.scss';
-import {LANGUAGES} from '../../utils'
+import { LANGUAGES, USER_ROLE } from '../../utils'
+import _ from 'lodash';
 
 class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            menuApp: []
+        }
+    }
     handleChangeLanguages = (language) => {
         this.props.changeLanguageAppRedux(language)
 
     }
+    componentDidMount() {
+        let { userInfo } = this.props;
+        let menu = []
+        if (userInfo && !_.isEmpty(userInfo)) {
+            let role = userInfo.roleId;
+            if (role === USER_ROLE.ADMIN) {
+                menu = adminMenu
+            }
+            if (role === USER_ROLE.DOCTOR) {
+                menu = doctorMenu
+            }
+        }
+        this.setState({
+            menuApp: menu
+        })
+        console.log("User infor", this.props.userInfo);
+    }
 
-     render() {
-        const { processLogout,language, userInfo } = this.props;
+
+    render() {
+        const { processLogout, language, userInfo } = this.props;
         // console.log("Check userInfo",userInfo);
 
         return (
             <div className="header-container">
                 {/* thanh navigator */}
                 <div className="header-tabs-container">
-                    <Navigator menus={adminMenu} />
+                    <Navigator menus={this.state.menuApp} />
                 </div>
                 <div className='languages'>
                     <span className='welcome'>
-                        <FormattedMessage id={'home-header.welcome'}/> 
+                        <FormattedMessage id={'home-header.welcome'} />
                         {userInfo && userInfo.firstName ? userInfo.firstName : ''}
                     </span>
-                    <span 
-                    className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}
-                    onClick={() =>this.handleChangeLanguages(LANGUAGES.VI)}>VN</span>
-                    <span 
-                    className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'} 
-                    onClick={() => this.handleChangeLanguages(LANGUAGES.EN)}>EN</span>
+                    <span
+                        className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}
+                        onClick={() => this.handleChangeLanguages(LANGUAGES.VI)}>VN</span>
+                    <span
+                        className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}
+                        onClick={() => this.handleChangeLanguages(LANGUAGES.EN)}>EN</span>
                     {/* nút logout */}
                     <div className="btn btn-logout" onClick={processLogout} title='Log out'>
                         <i className="fas fa-sign-out-alt"></i>
--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -2,13 +2,13 @@ export const adminMenu = [
     { //Quản lý người dùng
         name: 'menu.admin.manage-user',
         menus: [
-                        {
+            {
                 name: 'menu.admin.crud', link: '/system/user-manage'
-      
+
             },
             {
                 name: 'menu.admin.crud-redux', link: '/system/user-redux'
-      
+
             },
             {
                 name: 'menu.admin.manage-doctor', link: '/system/manage-doctor'
@@ -17,37 +17,52 @@ export const adminMenu = [
                 //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
                 // ]
             },
-            {
-                name: 'menu.admin.manage-admin', link: '/system/user-admin'
-      
+            // {
+            //     name: 'menu.admin.manage-admin', link: '/system/user-admin'
+
+            // },
+
+            { //Quản lý kế hoạch khám bệnh của bác sĩ
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
             },
         ]
     },
     { //Quản lý phòng khám
         name: 'menu.admin.clinic',
         menus: [
-                        {
+            {
                 name: 'menu.admin.manage-clinic', link: '/system/manage-clinic'
-      
+
             },
         ]
     },
     { //Quản lý chuyên khoa
         name: 'menu.admin.specialty',
         menus: [
-                        {
+            {
                 name: 'menu.admin.manage-specialty', link: '/system/manage-specialty'
-      
+
             },
         ]
     },
     { //Quản lý cẩm nang
         name: 'menu.admin.handbook',
         menus: [
-                        {
+            {
                 name: 'menu.admin.manage-handbook', link: '/system/manage-handbook'
-      
+
             },
         ]
     },
+];
+export const doctorMenu = [
+    {
+        name: 'menu.doctor.manage-schedule',
+        menus: [
+
+            { //Quản lý kế hoạch khám bệnh của bác sĩ
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
+            },
+        ]
+    }
 ];
